4c36cca1fcb7cc9d95f74bd0599f02c6d727feab
麦佃 <zhaozhenguo.zzg@alibaba-inc.com>
2021-08-28 18:02:11 +0800

    support版本升级

---
+++ app/src/main/java/cpps/Hello.java
@@ -0,0 +1,11 @@
+package cpps;
+
+/**
+ * Create by  [Rye]
+ * <p>
+ * at 2021/8/28 5:50 下午
+ */
+public class Hello {
+    public static native String sayHello();
+    //https://www.jianshu.com/p/8248ad152607
+}
